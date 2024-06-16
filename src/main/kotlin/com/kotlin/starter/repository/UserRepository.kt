88035c619f4e158837214878671553d9e5f7bd4d
UranusLin <aka611511@gmail.com>
2024-06-16 12:00:27 +0800

    feat: add some query

--- src/main/kotlin/com/kotlin/starter/repository/UserRepository.kt
+++ src/main/kotlin/com/kotlin/starter/repository/UserRepository.kt
@@ -3,4 +3,10 @@ package com.kotlin.starter.repository
 import com.kotlin.starter.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    // select * from users where username = ?
+    fun findByUsername(username: String): User?
+
+    // select * from users where email = ?
+    fun findByEmail(email: String): User?
+}
